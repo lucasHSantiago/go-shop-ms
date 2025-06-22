1661b3637a8c595076d4983d8e46f42e500ba8e1
Lucas H Santiago <lucashenriquesantiago@gmail.com>
2025-06-22 15:51:39 -0300

    update init scheme

--- product/migrate/000001_init_scheme.up.sql
+++ product/migrate/000001_init_scheme.up.sql
@@ -6,6 +6,5 @@ CREATE TABLE products (
     description VARCHAR,
     price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
     category_id UUID NOT NULL,
-    stock_quantity INTEGER NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0),
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
