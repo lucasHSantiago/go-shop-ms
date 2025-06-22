e2fa93d795a338079aa8980263f6939911059656
Lucas H Santiago <lucashenriquesantiago@gmail.com>
2025-06-22 15:51:19 -0300

    Add request and response handling utilities for JSON encoding and decoding

---
+++ foundation/request/request.go
@@ -0,0 +1,28 @@
+package request
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type validator interface {
+	Validate() error
+}
+
+// Decode reads the request body and decodes it into the provided value.
+func Decode(r *http.Request, val any) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	if err := decoder.Decode(val); err != nil {
+		return fmt.Errorf("unable to decode payload: %w", err)
+	}
+
+	if v, ok := val.(validator); ok {
+		if err := v.Validate(); err != nil {
+			return fmt.Errorf("unable to validate payload: %w", err)
+		}
+	}
+
+	return nil
+}
---
+++ foundation/response/response.go
@@ -0,0 +1,28 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Respond writes the provided data as a JSON response to the http.ResponseWriter.
+func Respond(w http.ResponseWriter, data any, statusCode int) error {
+	if statusCode == http.StatusNoContent {
+		w.WriteHeader(statusCode)
+		return nil
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if _, err := w.Write(jsonData); err != nil {
+		return err
+	}
+
+	return nil
+}
