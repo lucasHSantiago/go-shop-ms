e0cae5aa9643b3134a192c0727da8058a6d020a6
Lucas H Santiago <lucashenriquesantiago@gmail.com>
2025-06-22 21:49:58 -0300

    Added request responses

--- foundation/response/response.go
+++ foundation/response/response.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-// Respond writes the provided data as a JSON response to the http.ResponseWriter.
-func Respond(w http.ResponseWriter, data any, statusCode int) error {
+// Response writes the provided data as a JSON response to the http.ResponseWriter.
+func Response(w http.ResponseWriter, data any, statusCode int) error {
 	if statusCode == http.StatusNoContent {
 		w.WriteHeader(statusCode)
 		return nil
@@ -27,6 +27,17 @@ func Respond(w http.ResponseWriter, data any, statusCode int) error {
 	return nil
 }
 
+// RequestError creates a JSON response with an error message and a specific status code.
+func RequestError(w http.ResponseWriter, msg string, statusCode int) error {
+	return Response(w, map[string]any{"error": msg}, statusCode)
+}
+
+// BadRequest creates a JSON response with an error message for a bad request.
 func BadRequest(w http.ResponseWriter, err error) error {
-	return Respond(w, map[string]any{"error": err}, http.StatusBadRequest)
+	return Response(w, map[string]any{"error": err}, http.StatusBadRequest)
+}
+
+// InternalServerError creates a JSON response with an error message for a internal server error.
+func InternalServerError(w http.ResponseWriter, err error) error {
+	return Response(w, map[string]any{"error": err}, http.StatusInternalServerError)
 }
